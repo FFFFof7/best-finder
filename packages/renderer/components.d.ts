b7e20b0202cc3c12ab239cdde313f22b24b5c8f3
fanhuimin <fanhuimin.pn@unicloud.com>
2022-05-16 16:17:49 +0800

    chore: fix ts+pnpm type error for global components

--- packages/renderer/components.d.ts
+++ packages/renderer/components.d.ts
@@ -5,12 +5,9 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Counter: typeof import('./src/components/Counter.vue')['default']
-    Footer: typeof import('./src/components/Footer.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     StretchBox: typeof import('./src/components/stretchBox/index.vue')['default']
   }
 }
-
 export {}
